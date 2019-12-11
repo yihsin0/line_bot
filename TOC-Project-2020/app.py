d45c71ba239b0464fdc7b9b21f21948b8ee4c3ed
yihsin0 <46742022+yihsin0@users.noreply.github.com>
2019-12-11 19:47:21 +0800

    Add files via upload

---
+++ TOC-Project-2020/app.py
@@ -0,0 +1,130 @@
+import os
+import sys
+
+from flask import Flask, jsonify, request, abort, send_file
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+from fsm import TocMachine
+from utils import send_text_message
+
+load_dotenv()
+
+
+machine = TocMachine(
+    states=["user", "state1", "state2","state3","state4","state5","state6","start","chat","question1","one1","one2","one3","one4","two1","two2","two3","two4","question2"],
+    transitions=[
+        {"trigger": "advance","source": "start","dest": "state1","conditions": "is_going_to_state1",},
+        {"trigger": "advance","source": "user","dest": "start","conditions": "is_going_to_start",},
+        {"trigger": "advance","source": "start","dest": "state2","conditions": "is_going_to_state2",},
+        {"trigger": "advance","source": "state1","dest": "state3","conditions": "is_going_to_state3",},
+        {"trigger": "advance","source": "state1","dest": "state4","conditions": "is_going_to_state4",},
+        {"trigger": "advance","source": "state1","dest": "state5","conditions": "is_going_to_state5",},
+        {"trigger": "advance","source": "state1","dest": "state6","conditions": "is_going_to_state6",},
+        {"trigger": "advance","source": "start","dest": "chat","conditions": "is_going_to_chat",},
+        {"trigger": "advance","source": "chat","dest": "question1","conditions": "is_going_to_question1",},
+        {"trigger": "advance","source": "chat","dest": "question2","conditions": "is_going_to_question2",},
+        {"trigger": "advance","source": "question1","dest": "one1","conditions": "is_going_to_one1",},
+        {"trigger": "advance","source": "question1","dest": "one2","conditions": "is_going_to_one2",},
+        {"trigger": "advance","source": "question1","dest": "one3","conditions": "is_going_to_one3",},
+        {"trigger": "advance","source": "question1","dest": "one4","conditions": "is_going_to_one4",},
+        {"trigger": "advance","source": "question2","dest": "two1","conditions": "is_going_to_two1",},
+        {"trigger": "advance","source": "question2","dest": "two2","conditions": "is_going_to_two2",},
+        {"trigger": "advance","source": "question2","dest": "two3","conditions": "is_going_to_two3",},
+        {"trigger": "advance","source": "question2","dest": "two4","conditions": "is_going_to_two4",},
+        {"trigger": "go_back2","source": ["one1","one2","one3","one4"],"dest": "start"},
+        {"trigger": "go_back", "source": ["state1", "state2", "state3","state4","state5","state6","one1","question1","chat"], "dest": "user"},
+        
+
+    ],
+    initial="user",
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+app = Flask(__name__, static_url_path="")
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text)
+        )
+
+    return "OK"
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook_handler():
+
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if not isinstance(event.message.text, str):
+            continue
+        print(f"\nFSM STATE: {machine.state}")
+        print(f"REQUEST BODY: \n{body}")
+        response = machine.advance(event)
+        if response == False:
+            send_text_message(event.reply_token, "Not Entering any State")
+
+    return "OK"
+
+
+@app.route("/show-fsm", methods=["GET"])
+def show_fsm():
+    machine.get_graph().draw("fsm.png", prog="dot", format="png")
+    return send_file("fsm.png", mimetype="image/png")
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 8000)
+    app.run(host="0.0.0.0", port=port, debug=True)
---
+++ TOC-Project-2020/fsm.py
@@ -0,0 +1,271 @@
+from transitions.extensions import GraphMachine
+from linebot.models import *
+from utils import send_text_message
+from utils import send_image_url
+from utils import send_button_message
+from linebot.models import TemplateSendMessage, ButtonsTemplate, MessageTemplateAction
+
+class TocMachine(GraphMachine):
+    count=0
+
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(model=self, **machine_configs)
+    def is_going_to_start(self, event):
+        text = event.message.text
+        return text.lower() == "start"
+    def on_enter_start(self, event):
+        reply_token = event.reply_token
+        buttons_template1 = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+            title='無聊的話就來找我吧！',
+            text='選一個你想做的事！',
+            thumbnail_image_url='https://images.pexels.com/photos/3043798/pexels-photo-3043798.jpeg?cs=srgb&dl=person-holding-ball-night-lamp-while-sitting-3043798.jpg&fm=jpg',
+            actions=[
+                MessageTemplateAction(
+                    label='心理測驗',
+                    text='心理測驗'
+                ),
+                MessageTemplateAction(
+                    label='金頭腦',
+                    text='金頭腦'
+                )
+            ]    
+            )
+        )
+        send_button_message(reply_token, buttons_template1)
+    
+    def is_going_to_state1(self, event):
+        text = event.message.text
+        return text.lower() == "心理測驗"
+    
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
+
+        reply_token = event.reply_token
+        #send_text_message(reply_token, "enter state1")
+        
+        buttons_template = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+            title='測試一下你的幼稚程度吧!!',
+            text='如果你是童話故事中，想吃掉三隻小豬的大野狼，你認為使用哪一種方法可以容易吃掉他們？',
+            thumbnail_image_url='https://images.pexels.com/photos/3043798/pexels-photo-3043798.jpeg?cs=srgb&dl=person-holding-ball-night-lamp-while-sitting-3043798.jpg&fm=jpg',
+            actions=[
+                MessageTemplateAction(
+                    label='用煙把小豬勳到昏倒',
+                    text='用煙把小豬勳到昏倒'
+                ),
+                MessageTemplateAction(
+                    label='從煙囪或其他入口偷偷爬進屋',
+                    text='從煙囪或其他入口偷偷爬進屋'
+                ),
+                MessageTemplateAction(
+                    label='用槌子把門破壞闖入屋內',
+                    text='用槌子把門破壞闖入屋內'
+                ),
+                MessageTemplateAction(
+                    label='模仿豬媽媽的聲音騙小豬開門',
+                    text='模仿豬媽媽的聲音騙小豬開門'
+                )
+            ]    
+            )
+        )
+        send_button_message(reply_token, buttons_template)
+
+    def is_going_to_state2(self, event):
+        text = event.message.text
+        return text.lower() == "go to state2"
+    
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
+
+        reply_token = event.reply_token
+        send_image_url(reply_token, "https://images.pexels.com/photos/2691784/pexels-photo-2691784.jpeg?cs=srgb&dl=white-and-red-petaled-flowers-close-up-photography-2691784.jpg&fm=jpg")
+        self.go_back()
+    
+    def is_going_to_state3(self, event):
+        text = event.message.text
+        return text.lower() == "用煙把小豬勳到昏倒"
+    def on_enter_state3(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "你的幼稚指數99％：你簡直是活在童話世界中，幼稚到了極點 ! 成熟點好嗎~")
+        self.go_back()
+    
+    def is_going_to_state4(self, event):
+        text = event.message.text
+        return text.lower() == "從煙囪或其他入口偷偷爬進屋"
+    def on_enter_state4(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "幼稚指數55％：你自知已經半大不小，必須學習獨立自主，了解社會環境有善有惡，並非像童話世界一樣單純。你會在人生的路途中會慢慢的讓自己學習成長。")
+        self.go_back()
+    
+    def is_going_to_state5(self, event):
+        text = event.message.text
+        return text.lower() == "用槌子把門破壞闖入屋內"
+    def on_enter_state5(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "你的幼稚指數80％：看來你比較大男人或大女人，表面上很成熟，其實內心是非常幼稚的")
+        self.go_back()
+    
+    def is_going_to_state6(self, event):
+        text = event.message.text
+        return text.lower() == "模仿豬媽媽的聲音騙小豬開門"
+    def on_enter_state6(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "你的幼稚指數35％：你心智成熟！好棒棒！")
+        self.go_back()
+
+    def is_going_to_chat(self, event):
+        text = event.message.text
+        return text.lower() == "金頭腦"
+    def on_enter_chat(self, event):
+        reply_token = event.reply_token
+        buttons_template2 = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+            text='題目',
+            thumbnail_image_url='https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwif_sTdrqzmAhVPG6YKHdyyDSwQjRx6BAgBEAQ&url=https%3A%2F%2Fvemma888.pixnet.net%2Fblog%2Fpost%2F38742860-%25E6%2595%25B8%25E5%25AD%25B8%25E5%2595%258F%25E9%25A1%258C...%25E5%258B%2595%25E5%258B%2595%25E8%2585%25A6&psig=AOvVaw0wmVafyR6RZdo0OboOfapp&ust=1576111382381932',
+            actions=[
+                MessageTemplateAction(
+                    label='Q1',
+                    text='Q1'
+                ),
+                MessageTemplateAction(
+                    label='Q2',
+                    text='Q2'
+                )
+            ]    
+            )
+        )
+        send_button_message(reply_token, buttons_template2)
+
+    def is_going_to_question1(self, event):
+            text = event.message.text
+            return text.lower() == "q1"
+    
+    def on_enter_question1(self, event):
+        reply_token = event.reply_token
+        buttons_template2 = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+            text='Q1',
+            thumbnail_image_url='https://i.ibb.co/3RhP6jB/q1.jpg',
+            actions=[
+                MessageTemplateAction(
+                    label='1',
+                    text='q1.1'
+                ),
+                MessageTemplateAction(
+                    label='2',
+                    text='q1.2'
+                ),
+                MessageTemplateAction(
+                    label='3',
+                    text='q1.3'
+                ),
+                MessageTemplateAction(
+                    label='4',
+                    text='q1.4'
+                )
+            ]    
+            )
+        )
+        send_button_message(reply_token, buttons_template2)
+
+    def is_going_to_one1(self, event):
+        text = event.message.text
+        return text.lower() == "q1.1"
+    def on_enter_one1(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "答錯了～若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_one2(self, event):
+        text = event.message.text
+        return text.lower() == "q1.2"
+    def on_enter_one2(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "答錯了～若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_one3(self, event):
+        text = event.message.text
+        return text.lower() == "q1.3"
+    def on_enter_one3(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "答錯了～ 若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_one4(self, event):
+        text = event.message.text
+        return text.lower() == "q1.4"
+    def on_enter_one4(self, event):
+        reply_token = event.reply_token
+        #count = count + 10
+        send_text_message(reply_token, "恭喜答對!! 若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+
+
+    def is_going_to_question2(self, event):
+            text = event.message.text
+            return text.lower() == "q2"
+    
+    def on_enter_question2(self, event):
+        reply_token = event.reply_token
+        buttons_template3 = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+            text='Q2',
+            thumbnail_image_url='<a href="https://i.ibb.co/SrPF2QV/61680001o72r9o05op2o.jpg',
+            actions=[
+                MessageTemplateAction(
+                    label='2',
+                    text='q2.1'
+                ),
+                MessageTemplateAction(
+                    label='3',
+                    text='q2.2'
+                ),
+                MessageTemplateAction(
+                    label='5',
+                    text='q2.3'
+                ),
+                MessageTemplateAction(
+                    label='7',
+                    text='q2.4'
+                )
+            ]    
+            )
+        )
+        send_button_message(reply_token, buttons_template3)
+
+    def is_going_to_two1(self, event):
+        text = event.message.text
+        return text.lower() == "q2.1"
+    def on_enter_two1(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "答錯了～若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_two2(self, event):
+        text = event.message.text
+        return text.lower() == "q2.2"
+    def on_enter_two2(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "恭喜答對!! 若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_two3(self, event):
+        text = event.message.text
+        return text.lower() == "q2.3"
+    def on_enter_two3(self, event):
+        reply_token = event.reply_token
+        send_text_message(reply_token, "答錯了～ 若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+    def is_going_to_two4(self, event):
+        text = event.message.text
+        return text.lower() == "q2.4"
+    def on_enter_two4(self, event):
+        reply_token = event.reply_token
+        #count = count + 10
+        send_text_message(reply_token, "答錯了～ 若要繼續挑戰請輸入：金頭腦")
+        self.go_back2()
+
+    
+    
